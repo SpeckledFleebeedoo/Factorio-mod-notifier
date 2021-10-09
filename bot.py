495c66308a930b0dbf8137dd4570e234614ac5d3
SpeckledFleebeedoo <datema150@gmail.com>
2021-10-09 21:37:21 +0200

    No more storing 2 lists

--- bot.py
+++ bot.py
@@ -13,13 +13,14 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    client.ModLists = deque([botfunctions.getMods()], 2)
+    client.ModList = botfunctions.getMods()
     check_mod_updates.start()
 
 @tasks.loop(minutes=10)
 async def check_mod_updates():
-    client.ModLists.append(botfunctions.getMods())
-    updatedList = botfunctions.checkUpdates(client.ModLists[0], client.ModLists[1])
+    newModList = botfunctions.getMods()
+    updatedList = botfunctions.checkUpdates(client.ModList, newModList)
+    client.ModList = newModList
     if updatedList is not None:
         output = botfunctions.writeMessage(updatedList)
         channel = client.get_channel(CHANNEL)
