59e07a34e849df58f306b69e813df6832d67e536
SpeckledFleebeedoo <datema150@gmail.com>
2022-05-04 19:42:50 +0200

    Significant rewrite (unfinished), migration to cogs

--- botfunctions.py
+++ botfunctions.py
@@ -36,129 +36,19 @@ async def firstStart(guilds) -> list:
         cur.executemany("INSERT OR IGNORE INTO mods VALUES (?, ?, ?, ?, ?)", mods)
         con.commit()
 
+# async def main():
+#     """
+#     Executed when this file is run. Will run through all functions for testing purposes.
+#     """
+#     updatelist = await firstStart()
+#     if updatelist:
+#         for mod, tag in updatelist:
+#             name = mod[0]
+#             title = mod[2]
+#             owner = mod[3]
+#             version = mod[4]
+#             thumbnail = getThumbnail(name)
+#             print(title, owner, version, thumbnail)
 
-async def addGuild(guildID: int):
-    cur.execute("INSERT OR IGNORE INTO guilds VALUES (?, ?, ?)", (str(guildID), None, None))
-    con.commit()
-
-async def removeGuild(guildID: int):
-    cur.execute("DELETE FROM guilds WHERE id = (?)", [str(guildID)])
-    con.commit()
-
-async def setChannel(guildID: int, channelID: int):
-    cur.execute("UPDATE guilds SET updates_channel = (?) WHERE id = (?)", [str(channelID), str(guildID)])
-    con.commit()
-
-async def getModRole(roleID: int) -> str:
-    roles = cur.execute("SELECT modrole FROM guilds WHERE id = (?)", [str(roleID)]).fetchall()
-    return roles[0][0]
-
-async def setModRole(guildID: int, roleID: int):
-    cur.execute("UPDATE guilds SET modrole = (?) WHERE id = (?)", [str(roleID), str(guildID)])
-    con.commit()
-
-async def checkUpdates():
-    """
-    Iterates through pages of recently updated mods until unchanged mods are found.
-
-    Returns a list of [name, release date, title, owner, version]
-    """
-    modupdated = True
-    i = 1
-    updatelist = []
-    while modupdated == True:
-        url = f"https://mods.factorio.com/api/mods?page_size=10&page={i}&sort=updated_at&sort_order=desc"
-        try:
-            mods = await getMods(url)
-        except ConnectionError:
-            break
-        updatedmods = await compareMods(mods)
-        if updatedmods != []:
-            updatelist += updatedmods
-        i += 1
-        if len(updatedmods) != 10:
-            modupdated = False
-    return updatelist
-
-async def getMods(url: str) -> list:
-    """
-    Grabs the list of all mods from the API page and filters out the relevant entries. 
-    Returns a list of mods, each following the format [name, release date, title, owner, version]
-    """
-    response = requests.get(url)
-    if response.status_code == 200:
-        results = response.json()['results']
-        mods = [[mod["name"], mod["latest_release"]["released_at"], mod["title"], mod["owner"], mod["latest_release"]["version"]] for mod in results if mod.get('latest_release') is not None]
-        return mods
-    else:
-        raise ConnectionError("Failed to retrieve mod list")
-
-async def compareMods(mods: list) -> list:
-    """
-    Compares mods in list to entries stored in database. Sends list of updated mods to messager. 
-
-    Returns a list of [name, release date, title, owner, version], tag
-    """
-    updatedmods = []
-    for mod in mods:
-        existing_entry = cur.execute("SELECT * FROM mods WHERE name=:name", {"name": mod[0]}).fetchall()
-        if existing_entry == []:
-            updatedmods.append([mod, "n"])
-            cur.execute("INSERT INTO mods VALUES (?, ?, ?, ?, ?)", mod)
-        elif existing_entry[0][4] != mod[4]:
-            updatedmods.append([mod, "u"])
-            cur.execute("INSERT OR REPLACE INTO mods VALUES (?, ?, ?, ?, ?)", mod)
-    con.commit()
-    return updatedmods
-
-async def make_safe(string: str) -> str:
-    """
-    Escapes formatting to avoid unwanted behaviour in Discord messages.
-    """
-    return string.replace("_", "\_").replace("*", "\*").replace("~","\~").replace("@", "@​\u200b")
-
-async def getThumbnail(name: str) -> str:
-    """
-    Finds the thumbnail for the specified mods.
-
-    Returns either the URL or None if no thumbnail exists or the connection fails.
-    """
-    url = f"https://mods.factorio.com/api/mods/{name}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        json = response.json()
-        if "thumbnail" in json:
-            thumbnailraw = json["thumbnail"]
-        else:
-            return None
-        if thumbnailraw != "/assets/.thumb.png":
-            thumbnailURL = "https://assets-mod.factorio.com" + thumbnailraw
-            return thumbnailURL
-        else:
-            return None
-    else:
-        return None
-
-async def getChannels() -> list:
-    """
-    Gets and returns a list of all set channel IDs
-    """
-    channels = cur.execute("SELECT updates_channel FROM guilds WHERE updates_channel IS NOT NULL").fetchall()
-    return channels
-
-async def main():
-    """
-    Executed when this file is run. Will run through all functions for testing purposes.
-    """
-    updatelist = await firstStart()
-    if updatelist:
-        for mod, tag in updatelist:
-            name = mod[0]
-            title = mod[2]
-            owner = mod[3]
-            version = mod[4]
-            thumbnail = getThumbnail(name)
-            print(title, owner, version, thumbnail)
-
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
